2501d9649a1de6de966d433f7b30d98411b50c9b
Marie_alvrz <marie852006@gmail.com>
2026-04-19 15:32:06 -0400

    Uploading Dtos

---
+++ src/Backend/SewControl.Application/Dtos/Encargos/PrendaDto.cs
@@ -0,0 +1,31 @@
+﻿using SewControl.Domain.Entities.Encargos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SewControl.Application.Dtos.Encargos;
+
+public class PrendaDto
+{
+    public int Id { get; set; }
+    public TipoPrenda Tipo { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public string? Tela { get; set; }
+    public string? Color { get; set; }
+    public string? Talla { get; set; }
+    public string? Observaciones { get; set; }
+    public int EncargoId { get; set; }
+}
+
+public class CreatePrendaDto
+{
+    public TipoPrenda Tipo { get; set; }
+    public string Descripcion { get; set; } = string.Empty;
+    public string? Tela { get; set; }
+    public string? Color { get; set; }
+    public string? Talla { get; set; }
+    public string? Observaciones { get; set; }
+    public int EncargoId { get; set; }
+}
---
+++ src/Backend/SewControl.Application/Dtos/Usuarios/ClienteDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SewControl.Application.Dtos.Usuarios;
+
+public class ClienteDto
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? Direccion { get; set; }
+    public DateTime FechaRegistro { get; set; }
+    public int TotalEncargos { get; set; }
+}
+
+public class CreateClienteDto
+{
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string? Email { get; set; }
+    public string? Direccion { get; set; }
+}
