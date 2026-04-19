13390394f0cd4ba41d7b146cae9bf49cec4ebc3f
Marie_alvrz <marie852006@gmail.com>
2026-04-19 14:48:11 -0400

    Creating the Entities

---
+++ src/SewControl.API/SewControl.Domain/Entities/Usuarios/Costurera.cs
@@ -0,0 +1,20 @@
+﻿using SewControl.Domain.Entities.Encargos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SewControl.Domain.Entities.Usuarios;
+public class Costurera
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = string.Empty;
+    public string Apellido { get; set; } = string.Empty;
+    public string Telefono { get; set; } = string.Empty;
+    public string? Especialidad { get; set; }
+    public bool Activa { get; set; } = true;
+    public bool IsDeleted { get; set; } = false;
+
+    public ICollection<Encargo> Encargos { get; set; } = new List<Encargo>();
+}
