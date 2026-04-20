5121014e900ba404723f1ed396527efceadcc68d
Marie_alvrz <marie852006@gmail.com>
2026-04-19 22:20:40 -0400

    creating and defining Program and dependency injection

--- src/Backend/SewControl.API/Program.cs
+++ src/Backend/SewControl.API/Program.cs
@@ -1,23 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using SewControl.Application.MappingProfile;
+using SewControl.Application.Services;
+using SewControl.Infrastructure.Repositories;
+using SewControl.Persistence.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<TallerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<DbContext>(sp => sp.GetRequiredService<TallerContext>());
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<EncargoService>();
+builder.Services.AddScoped<UsuariosService>();
+
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile<SewControlMappingProfile>();
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
